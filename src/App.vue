bcfa6f4418fd86f8472a526279063c582ecfc0b4
Ijeoma Jahsway <uchennamebijay@gmail.com>
2024-09-11 16:25:57 +0100

    Real Time chat app with Chat Engine

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AuthPage from './AuthPage/Index.vue';
+import ChatsPage from './ChatPage/Index.vue';
+</script>
+
+<template>
+  <AuthPage v-if="!user" @onAuth="handleAuth" />
+  <ChatsPage
+    v-else
+    v-bind:username="user.username"
+    v-bind:secret="user.secret"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: undefined,
+    };
+  },
+  methods: {
+    handleAuth(user) {
+      this.user = user;
+    },
+  },
+};
+</script>
